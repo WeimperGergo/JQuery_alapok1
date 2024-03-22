491680de66f387333a0dcd70f5dafbc6fd8e10da
Weimper Gergő Zsombor (SZF_2023_2025) <weimper.gergo.zsombor@diak.szamalk-szalezi.hu>
2024-03-22 11:15:55 +0100

    Órai munka

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function () {
+    elemekElerese1();
+    elemekElerese2();
+    elemekElerese3();
+});
+
+function elemekElerese1() {
+    /**Ide jön az első feladat */
+}
+function elemekElerese2() {
+    /**Ide jön az 2.  feladat */
+}
+function elemekElerese3() {
+    /**Ide jön az 3. feladat */
+}
